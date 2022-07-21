00eea3e5d46d77290650377e8adffe669be99a9a
Beau Jones <beautyler96@gmail.com>
2022-07-20 20:02:53 -0700

    Event listerner, UI update

--- rps.js
+++ rps.js
@@ -13,7 +13,7 @@ let computerSelection;
 
 /* Checking to see if the weapon the player chose is valid -- function */
 
-function checkWeapon() {
+/* function checkWeapon() {
     if (playerSelection == 'rock' || playerSelection == 'paper' || playerSelection == 'scissors') {
         return;
     } else {
@@ -23,16 +23,17 @@ function checkWeapon() {
     return playerSelection;
 }
 
+*/
 
 /* Play a round of rock, paper, scissors and keep track of the winners */
 
 let playerWins = 0;
 let computerWins = 0;
 
-function playARound() {
+/* function playARound() {
     computerPlay(compOptions);
     computerSelection = computerPlay(compOptions);    
-    playerSelection = prompt("Choose your weapon, Champion!").toLowerCase();
+    // playerSelection = prompt("Choose your weapon, Champion!").toLowerCase(); 
     checkWeapon(playerSelection);
 
     if (playerSelection == 'rock' && computerSelection == 'paper') {
@@ -63,7 +64,7 @@ function playARound() {
 /* Play a series of rounds and keep track of the winners */
 
 
-function game() {
+/* function game() {
     while (playerWins < 5 && computerWins < 5) {
         playARound();
         if (playerSelection === computerSelection) {
@@ -78,11 +79,133 @@ function game() {
 
 game();
 
+*/
+
 function declareWinner() {
-    if (playerWins == 5) {
-        console.log("You are the BIG winner!")
-    } else {
-        console.log("You lost to a machine. You make me sick.")
+    if (playerWins !== 5 & computerWins !== 5) {
+        return;
+    } else if (playerWins == 5) {
+        const p = document.createElement('p');
+        p.textContent = "YOU WIN!"
+        div.appendChild(p);
+    } else if (computerWins == 5) {
+        const p = document.createElement('p');
+        p.textContent = "MACHINE WINS"
+        div.appendChild(p);
+
     }
 }
+
+
+
+// Revisiting Rock Paper Scissors, using DOM and Event Listeners
+
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => button.addEventListener('click', playARound));
+
+function playARound(e) {
+    computerPlay(compOptions);
+    computerSelection = computerPlay(compOptions);    
+    playerSelection = this.id;
+    /* checkWeapon(playerSelection); */
+
+const div = document.querySelector('#results');
+
+    
+    if (playerSelection == 'rock' && computerSelection == 'paper') {
+            computerWins++;
+        const p = document.createElement('p');
+        p.textContent = "You chose, poorly." + " Player: " + playerWins + " Computer: " + computerWins;
+        div.appendChild(p);
+        //div.textContent += "You chose, poorly." + " Player: " + playerWins + " Computer: " + computerWins;
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+            playerWins++;
+        const p = document.createElement('p');
+        p.textContent = "You chose, wisely." + " Player: " + playerWins + " Computer: " + computerWins;
+        div.appendChild(p);
+        //div.textContent += "You chose, wisely." + " Player: " + playerWins + " Computer: " + computerWins;
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+            computerWins++;
+        const p = document.createElement('p');
+        p.textContent = "You chose, poorly." + " Player: " + playerWins + " Computer: " + computerWins;
+        div.appendChild(p);
+        //div.textContent += "You chose, poorly." + " Player: " + playerWins + " Computer: " + computerWins;
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+            playerWins++;
+        const p = document.createElement('p');
+        p.textContent = "You chose, wisely." + " Player: " + playerWins + " Computer: " + computerWins;
+        div.appendChild(p);
+        //div.textContent += "You chose, wisely." + " Player: " + playerWins + " Computer: " + computerWins;
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+            computerWins++;
+        const p = document.createElement('p');
+        p.textContent = "You chose, poorly" + " Player: " + playerWins + " Computer: " + computerWins;
+        div.appendChild(p);
+        //div.textContent += "You chose, poorly" + " Player: " + playerWins + " Computer: " + computerWins;
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+            playerWins++;
+        const p = document.createElement('p');
+        p.textContent = "You chose, wisely." + " Player: " + playerWins + " Computer: " + computerWins;
+        div.appendChild(p);
+        //div.textContent += "You chose, wisely." + " Player: " + playerWins + " Computer: " + computerWins;
+    } else {
+            const p = document.createElement('p');
+        p.textContent = "You have tied this round." + " Player: " + playerWins + " Computer: " + computerWins;
+        div.appendChild(p);
+        //div.textContent += "You have tied this round." + " Player: " + playerWins + " Computer: " + computerWins;
+    
+    }
+
 declareWinner();
+
+}
+
+
+
+function declareWinner() {
+
+const div = document.querySelector('#results');
+
+    if (playerWins !== 5 && computerWins !== 5) {
+        return;
+    } else if (playerWins == 5) {
+        const p = document.createElement('p');
+        p.textContent = "YOU WIN!";
+        div.appendChild(p);
+    } else if (computerWins == 5) {
+        const p = document.createElement('p');
+        p.textContent = "MACHINE WINS";
+        div.appendChild(p);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
